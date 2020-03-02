59ec0ef086f8d02847957280270d65dd852c5bbc
Marcel Faßmann <marcel.fassmann@dff-solutions.de>
2020-03-02 13:26:09 +0100

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Test");
+            }
+        }
+    }
+}
